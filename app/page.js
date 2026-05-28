88d1df2dde5dbc44ec079ab641f14a52b32d035b
Shalineth <shalinethcureg45@gmail.com>
2026-05-28 16:25:43 +0800

    add homepage

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding: '50px', textAlign: 'center'}}>
+      <h1>Welcome to My Store!</h1>
+      <p>Deployed successfully on Vercel 🚀</p>
+    </main>
+  )
+}
