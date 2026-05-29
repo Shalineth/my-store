aa0be0b0e0a8b844c2153a82eb2cd47244998a58
Shalineth <shalinethcureg45@gmail.com>
2026-05-29 09:51:33 +0800

    Create products

---
+++ app/products.js
@@ -0,0 +1,20 @@
+export const products = [
+  { 
+    id: 1, 
+    name: 'Tshirt Black', 
+    price: 299, 
+    image: 'https://via.placeholder.com/300x300/000/ffffff?text=Tshirt' 
+  },
+  { 
+    id: 2, 
+    name: 'Cap Snapback', 
+    price: 399, 
+    image: 'https://via.placeholder.com/300x300/333/ffffff?text=Cap' 
+  },
+  { 
+    id: 3, 
+    name: 'Tote Bag', 
+    price: 249, 
+    image: 'https://via.placeholder.com/300x300/666/ffffff?text=Bag' 
+  }
+]
