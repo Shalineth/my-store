61a2db5a2f6044c1646137b2c4c140c6b4d05b03
Shalineth <shalinethcureg45@gmail.com>
2026-05-29 10:23:27 +0800

    fix image

--- app/products.js
+++ app/products.js
@@ -1,4 +1,14 @@
 export const products = [
-  { id: 1, name: 'Shirt', price: 299, image: 'https://via.placeholder.com/300' },
-  { id: 2, name: 'Short', price: 399, image: 'https://via.placeholder.com/300' }
+  { 
+    id: 1, 
+    name: 'Shirt', 
+    price: 299, 
+    image: 'https://picsum.photos/300/300?random=1' 
+  },
+  { 
+    id: 2, 
+    name: 'Short', 
+    price: 399, 
+    image: 'https://picsum.photos/300/300?random=2' 
+  }
 ]
