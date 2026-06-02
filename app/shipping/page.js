289c6f4a8aca346ede80a795b7980c8aedd20f85
Shalineth <shalinethcureg45@gmail.com>
2026-06-02 13:50:49 +0800

    change address code

--- app/shipping/page.js
+++ app/shipping/page.js
@@ -12,9 +12,10 @@ export default function ShippingPage() {
 
         <h3>📦 Delivery Time</h3>
         <ul>
-          <li><strong>TUGUEGARAO CITY:</strong> 2-3 business days</li>
-          <li><strong>CAGAYAN:</strong> 3-5 business days</li>
-       
+          <li><strong>Tuguegarao City:</strong> 2-3 business days</li>
+          <li><strong>Luzon:</strong> 3-5 business days</li>
+          <li><strong>Visayas & Mindanao:</strong> 5-7 business days</li>
+        </ul>
 
         <h3 style={{ marginTop: '30px' }}>💰 Shipping Rates</h3>
         <table style={{ width: '100%', borderCollapse: 'collapse', marginTop: '15px' }}>
